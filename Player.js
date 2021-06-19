cc796d9b106f29db5816a7b5707f4108ae329ba1
sumegha1803 <78134736+sumegha1803@users.noreply.github.com>
2021-06-20 00:12:29 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,57 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+	
+	moveLeft()
+	{
+		this.spt.x=this.spt.x-12;
+
+	}
+
+	moveRight()
+	{
+		this.spt.x=this.spt.x+12;
+
+	}
+
+	moveUp()
+	{
+		this.spt.velocityY=-20;
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
